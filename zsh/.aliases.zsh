e9b0690a04ce4a8ea009e0b372ee07a147f0ed55
Uladzislau Volchyk <uladzislau.volchyk@gmail.com>
2020-09-20 09:51:23 +0300

    feat: config iterm & more aliases

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -14,15 +14,20 @@ alias gs='git status'
 alias gl='git log --pretty=oneline --graph'
 alias gb='git branch'
 alias ga='git add'
+alias gm='git merge'
 alias gch='git checkout'
-alias gre='git reset'
 alias gps='git push'
 alias gpl='git pull'
 alias gcm='git commit -m'
 alias gcl='git clone'
-
+alias grb='git rebase'
+alias grv='git revert'
+alias grs='git reset'
 
 # python
 alias py='python3'
 alias py2='python'
 
+
+# xcode
+alias xc='xed .'
